24ea6e3f945d83fc6bc3a6cadd563ce773aab987
elisha <elishamutang@outlook.com>
2024-09-02 23:48:48 +0800

    [misc] Misc stuff.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import icon from "astro-icon";
+
 export default defineConfig({
   site: "https://elishamutang.netlify.app/",
-  integrations: [preact()],
+  integrations: [preact(), icon()],
 });
