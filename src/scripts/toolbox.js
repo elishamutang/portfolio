716d838c1a7a1f99b27a4a354defeb8a717b55ec
elishamutang <elishamutang@outlook.com>
2025-05-21 17:29:35 +0800

    update: Updated code to get colored icons.

--- src/scripts/toolbox.js
+++ src/scripts/toolbox.js
@@ -6,16 +6,34 @@
 export default function generateToolIcon() {
   const icons = import.meta.glob("../icons/tools/*.astro", { eager: true });
 
-  const regex = /\/([^\/]+)Icon\.astro$/;
-  const tools = [];
+  const baseIconRegex = /\/([^\/]+)Icon\.astro$/;
+  const coloredIconRegex = /\/([^\/]+)IconColor\.astro$/;
 
-  Object.entries(icons).forEach(([key, value]) => {
-    const match = key.match(regex);
+  const tools = {};
+  const added = new Set()
 
-    if (match) {
-      tools.push({ name: match[1], component: value });
+  Object.entries(icons).forEach(([key, value], idx) => {
+    const baseMatch = key.match(baseIconRegex);
+    const coloredMatch = key.match(coloredIconRegex)
+
+    const match = baseMatch ? baseMatch : coloredMatch
+
+    if(match) {
+
+      if(!added.has(match[1])) {
+        tools[match[1]] = {
+          base: value
+        }
+
+        added.add(match[1])
+      } else {
+        tools[match[1]] = {...tools[match[1]], colored: value}
+      }
+
     }
   });
+
+  console.log(tools)
 
   return tools;
 }
